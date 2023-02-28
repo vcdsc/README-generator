d7d42dd11b3d96723b500cb51dfbbe093e2b89f0
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-02-28 23:05:06 +0000

    add license badge
    when user selects a license through the inquirer input, this should generate a corresponding badge in the newly generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,9 +2,48 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+![${data.license}](https://img.shields.io/badge/license-${data.license}-blue)
+
 ## Description
 
-${data.description}`;
+${data.description}
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [License](#License)
+- [Questions](#Questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## License
+
+This project is licensed under the [${data.license} License](https://choosealicense.com/licenses/).
+
+## Questions
+
+Should you have any queries regarding this project, here is how you can reach me:
+
+- [${data.github}](https://github.com/${data.github})
+- ${data.email}
+`;
 }
 
 module.exports = generateMarkdown;
